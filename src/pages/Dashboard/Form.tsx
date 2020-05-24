9307e41e6e591ca4188fb699ff0bb1a4e571128b
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-05-25 00:16:51 +0800

    feat: add formily

--- src/pages/Dashboard/Form.tsx
+++ src/pages/Dashboard/Form.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import { Input } from '@alifd/next';
 
-export default () => <><Input /></>
+// export default () => <><Input /></>
 
-
-// import { Form, FormItem, FormButtonGroup, Submit, Reset } from '@formily/next';
-
-// const Dashboard = () => {
-//   return (
-//     <Form
-//       onSubmit={values => {
-//         console.log(values)
-//       }}
-//     >
-//       <FormItem
-//         name="name"
-//         label="Name"
-//         placeholder="Input Name"
-//         component={Input}
-//       />
-//       <FormButtonGroup>
-//         <Submit>查询</Submit>
-//         <Reset>重置</Reset>
-//       </FormButtonGroup>
-//     </Form>
-//   );
-// };
-
-// export default Dashboard;
+import { Form, FormItem, FormButtonGroup, Submit, Reset } from '@formily/next';
+export default () => {
+  return (
+    <Form
+      onSubmit={values => {
+        console.log(values)
+      }}
+    >
+      <FormItem
+        name="name"
+        label="Name"
+        placeholder="Input Name"
+        component={Input}
+      />
+      <FormButtonGroup>
+        <Submit>查询</Submit>
+        <Reset>重置</Reset>
+      </FormButtonGroup>
+    </Form>
+  );
+};
